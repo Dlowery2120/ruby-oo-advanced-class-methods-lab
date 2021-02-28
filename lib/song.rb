98a55b57938cf5d32af9d65bb2d4c5e51290f289
Devante Lowery <dlowery2120@gmail.com>
2021-02-28 11:29:49 -0600

    commit tree d09ea79b0b06269a5a0828564f816fcaff63a9f5

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,19 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize (name, artist_name)
+    @name = name
+    @artist_name = artist_name
+    @@all << self
+  end
+  def self.create
+    puts "created song"
+  end
   def self.all
-    @@all
+    @@all  
   end
 
   def save
@@ -11,3 +21,12 @@ class Song
   end
 
 end
+
+song = Song.create
+Song.all.include?(song)
+# song = Song.new_by_name("The Middle")
+# song.name
+
+
+binding.pry 
+0
